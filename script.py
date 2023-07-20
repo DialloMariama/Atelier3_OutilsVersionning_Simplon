9c01fb9e7368b9aac26fce0df71e4fc46e05eed5
DialloMariama <79838601+DialloMariama@users.noreply.github.com>
2023-07-20 16:32:57 +0000

    Update script.py

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ def division(a, b):
     else:
         return "Division par zéro n'est pas autorisée."
 
-num1 = 10
-num2 = 2
+num1 = 11
+num2 = 25
 result = division(num1, num2)
 print("Résultat de la division :", result)
